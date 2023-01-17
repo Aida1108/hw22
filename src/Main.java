b90c6453e6d9933660c24805441235548ef27a82
Aida1108 <mi.aida@yandex.ru>
2023-01-17 18:59:58 +0500

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Car lada = new Car("Lada", "Granta", -1.7, " ", -2015, "Россия",
+               "hatchback", false, null, "h585ло185", -6,
+               new Car.Key(true, true));
+       Car.Key key = new Car.Key(true,true);
+       System.out.println(lada);
+
     }
 }
